1f5649e86fc6a3f6b4840634d7b679ac696781a7
Hyusein Hyusein <hyusein.ali.hyusein98@gmail.com>
2023-12-17 09:16:44 +0100

    Added localisation file

---
+++ RestoRaterApp/Lingo/Lingo.swift
@@ -0,0 +1,16 @@
+//
+//  Lingo.swift
+//  RestoRaterApp
+//
+//  Created by user249550 on 12/17/23.
+//
+
+import Foundation
+
+// All labels used in the app
+// This setup allows for an easy localization of the app and addition of multple languages
+struct Labels {
+    static let commandCode: String = String(localized: "current_command_code")
+
+
+}
