5e78bc1d93a55825f39e1ffe531ae7b167e0b5c0
Hyusein Hyusein <hyusein.ali.hyusein98@gmail.com>
2023-12-17 09:26:13 +0100

    Added strings for ProfileView

--- RestoRaterApp/Lingo/Lingo.swift
+++ RestoRaterApp/Lingo/Lingo.swift
@@ -9,8 +9,15 @@ import Foundation
 
 // All labels used in the app
 // This setup allows for an easy localization of the app and addition of multple languages
-struct Labels {
-    static let commandCode: String = String(localized: "current_command_code")
-
-
+struct Lingo {
+    // ProfileView labels
+       static let profileViewTitle = String(localized: "profile_view_title")
+       static let profileViewNoUserLoggedIn = String(localized: "profile_view_no_user_logged_in")
+       static let profileViewLogoutButton = String(localized: "profile_view_logout_button")
+       static let profileViewNameTitle = String(localized: "profile_view_name_title")
+       static let profileViewEmailTitle = String(localized: "profile_view_email_title")
+       static let profileViewRoleTitle = String(localized: "profile_view_role_title")
+       static let profileViewAdminRole = String(localized: "profile_view_admin_role")
+       static let profileViewRegularUserRole = String(localized: "profile_view_regular_user_role")
+       
 }
